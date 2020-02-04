368065954380573b27ec12df00e71837d88681f5
hwalters <hwalters@fusetg.com>
2020-02-03 21:35:24 -0500

    Removewd duration b ullshit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -24,8 +24,8 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \Barryvdh\Cors\HandleCors::class,
-        SetTimerMiddleware::class,
-        AppendDurationMiddleware::class,
+        // SetTimerMiddleware::class,
+        // AppendDurationMiddleware::class,
     ];
 
     /**
