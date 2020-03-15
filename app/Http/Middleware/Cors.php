d93be36bfb96a60bae2a8ed0548c2e25bea49a8d
Hassan Bazzi <hbazzi@nexcess.net>
2020-03-15 17:38:57 -0400

    cors v3

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -1,12 +1,15 @@
 <?php
 namespace App\Http\Middleware;
 use Closure;
+use Illuminate\Support\Facades\Log;
+
 
 class Cors {
   public function handle($request, Closure $next)
   {
+    Log::debug('cors');
     return $next($request)
-      ->header('Access-Control-Allow-Origin', '*')
+      ->header('Access-Control-Allow-Origin', 'https://app.aitimesheets.com')
       ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
       ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
   }
