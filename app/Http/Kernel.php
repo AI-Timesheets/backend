ea540fb835f1cb00843f17c12d67547a0bd9779f
Hassan Bazzi <hbazzi@nexcess.net>
2020-03-15 17:30:03 -0400

    cors v2

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -24,8 +24,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \Barryvdh\Cors\HandleCors::class,
-        \Spatie\Cors\Cors::class
+        \App\Http\Middleware\Cors::class
         // SetTimerMiddleware::class,
         // AppendDurationMiddleware::class,
     ];
@@ -71,6 +70,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'cors' => \App\Http\Middleware\Cors::class, // <-- add this line
     ];
 
     /**
