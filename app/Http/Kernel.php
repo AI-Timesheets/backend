c2cf92afc9cdba9bd4bb35ca9b3f6c508e1bc102
Hassan Bazzi <hbazzi@nexcess.net>
2020-03-15 17:22:40 -0400

    add cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -25,6 +25,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \Barryvdh\Cors\HandleCors::class,
+        \Spatie\Cors\Cors::class
         // SetTimerMiddleware::class,
         // AppendDurationMiddleware::class,
     ];
