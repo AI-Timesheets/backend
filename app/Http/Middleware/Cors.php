7e49db8ca48e22c19cdd1e08b8fe778ef1300412
Hassan Bazzi <hbazzi@nexcess.net>
2020-03-15 17:43:52 -0400

    cors cleanup

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -7,9 +7,8 @@ use Illuminate\Support\Facades\Log;
 class Cors {
   public function handle($request, Closure $next)
   {
-    Log::debug('cors');
     return $next($request)
-      ->header('Access-Control-Allow-Origin', 'https://app.aitimesheets.com')
+      ->header('Access-Control-Allow-Origin', env('CORS_DOMAINS', 'https://app.aitimesheets.com'))
       ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
       ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
   }
